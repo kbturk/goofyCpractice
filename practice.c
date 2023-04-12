2fc3ae9d2e38d8b9bd7585f8943e71c4d1b2b176
Katherine Turk <kbturk87@gmail.com>
2023-04-12 11:57:12 -0500

    replace tabs with \t, backspace with \b, backslash with \

--- practice.c
+++ practice.c
@@ -6,25 +6,33 @@ int main() {
     int c, b,t,nl;
     b = t = nl = 0;
     /*replace long blanks*/
-    bool blank;
-    blank = false;
+    bool written;
+    written = false;
+    /*replace tab with \t, backspace by \b, and backslash by \\*/
+
     while ((c = getchar()) != EOF) {
         if (c == '\b') {
             ++b;
+            printf("\\b");
+            written = true;
         } else if (c == '\t') {
             ++t;
+            printf("\\t");
+            written = true;
         } else if (c == '\n') {
             ++nl;
-        }
-        if (c == ' ') {
-            if (!blank) {
+        } else if (c == '\\') {
+            printf("\\\\");
+            written = true;
+        } else if (c == ' ') {
+            if (!written) {
                 putchar(c);
             }
-            blank = true;
+            written = true;
         } else {
-            blank = false;
+            written = false;
         }
-        if (!blank) putchar(c);
+        if (!written) putchar(c);
     }
     printf("backspace count: %d, tab count: %d, newline count: %d", b, t, nl);
     return 0;
