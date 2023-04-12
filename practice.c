c51c47d1ff97d7f40b6293918d1307b5cb9a2418
Katherine Turk <kbturk87@gmail.com>
2023-04-12 11:41:57 -0500

    exercise 1-8

--- practice.c
+++ practice.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    long nc, nl, c;
-    nc = 0;
-    nl = 0;
+    /*variables to count blanks, tabs, and newlines*/
+    int c, b,t,nl;
+    b = t = nl = 0;
     while ((c = getchar()) != EOF) {
-        ++nc;
-        if (c == '\n')
+        if (c == '\b') {
+            ++b;
+        } else if (c == '\t') {
+            ++t;
+        } else if (c == '\n') {
             ++nl;
+        }
     }
-    printf("program char: %ld newline chars: %ld\n", nc, nl);
+    printf("backspace count: %d, tab count: %d, newline count: %d", b, t, nl);
     return 0;
 }
