93ade9681a7be28cc908909da2130f58adde7e54
Katherine Turk <kbturk87@gmail.com>
2023-04-12 11:47:45 -0500

    exercise 1-9 done

--- practice.c
+++ practice.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     /*variables to count blanks, tabs, and newlines*/
     int c, b,t,nl;
     b = t = nl = 0;
+    /*replace long blanks*/
+    bool blank;
+    blank = false;
     while ((c = getchar()) != EOF) {
         if (c == '\b') {
             ++b;
@@ -12,6 +16,15 @@ int main() {
         } else if (c == '\n') {
             ++nl;
         }
+        if (c == ' ') {
+            if (!blank) {
+                putchar(c);
+            }
+            blank = true;
+        } else {
+            blank = false;
+        }
+        if (!blank) putchar(c);
     }
     printf("backspace count: %d, tab count: %d, newline count: %d", b, t, nl);
     return 0;
