45ca378a773f564067322e8411782a3f119e5fc2
Katherine Turk <kbturk87@gmail.com>
2023-04-12 22:13:52 -0500

    string manipulation

--- practice.c
+++ practice.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
+#define MAXLINE 1000
 
-int power(int m, int n);
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+int stop(char line[]);
+void striptrailing(char to[], char from[]);
+void copyreverse(char to[], char from[]);
 
-/*test power function*/
+/*print the longest input line*/
 int main() {
-    int i;
-    for (int i = 0; i < 10; ++i)
-        printf("%d %d %d\n", i, power(2,i), power(3,i));
+    int len;
+    int max;
+    int diff;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    max = 0;
+
+    while ((len = getline(line, MAXLINE)) > 0) {
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+        if (len > 80) {
+            printf("long line detected:\n%s",line);
+        }
+    }
+    if (max > 0)
+        printf("longest line was: %d\n%s", max, longest);
+    else
+        printf("no lines contained characters!");
     return 0;
 }
 
-/*power: raise base to n-th power; n >=0*/
-int power(int base, int n) {
-    int i, p;
+int getline(char s[], int lim) {
 
-    p = 1;
-    for (i = 1; i <=n; ++i) p = p * base;
-    return p;
+    int c, i;
+
+    for (i=0; i<lim-1 && (c=getchar()) !=EOF && c != '\n'; ++i) s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
+
+void copy(char to[], char from[]) {
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
+
+
