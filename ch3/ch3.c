d0ab4642a06b54d358f634c241eef0dc4000bda4
Katherine Turk <kbturk87@gmail.com>
2023-04-21 22:10:30 -0500

    this feels like git whoring, but I did a couple examples from ch3

--- ch3/ch3.c
+++ ch3/ch3.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
+
+#define MAXLINE 1000
 
 int binsearch(int x,  int v[], int n);
 int binsearch2(int x, int v[], int n);
 int intlen2(int s[]);
+void escape(char s[], char t[]);//exercise 3-2
+void trap(char s[], char t[]);//exercise 3-2
 
 int main() {
     int v[] = {0,1,2,3,4,5,6,7,8,9,10};
     int n, bs1, bs2;
+    char t2[MAXLINE];
+    char t[MAXLINE];
 
     //exercise 3-1
+    //TODO: get the clock function to actually work & ask
+    //Derrick if there's a better way to profile this function
+    printf("\n---exercise 3-1---\n");
     n = 11;
     printf("%d\n", n);
     clock_t begin = clock();
@@ -23,7 +33,64 @@ int main() {
             bs1, time_spent,
             bs2, time_spent2);
 
+    printf("\n---exercise 3-2---\n");
+    escape("\t\t\tMOM!!!!!\n RUN!",t);
+    trap("\\t\\t\\tMOM!!!!\\n STOP!", t2);
     return 0;
+}
+
+void trap(char s[], char t[]) {
+    int i, j;
+    bool temp;
+    temp = false;
+
+    for (i = 0, j = 0; s[i] != '\0'; i++, j++) {
+        if (temp) {
+            switch (s[i]) {
+                case 'n':
+                case 't':
+                    t[j] = (s[i] == 't') ? '\t' : '\n';
+                    break;
+                default:
+                    break;
+            }
+            temp = false;
+        } else {
+            switch (s[i]) {
+                case '\\':
+                    temp = true;
+                    j--;
+                    break;
+                default:
+                    t[j] = s[i];
+            }
+        }
+    }
+
+    t[j] = '\0';
+    printf("s: %s\nt: %s\n",s,t);
+}
+
+void escape(char s[], char t[]) {
+    /*converts characters like newline and tab into visible
+     * escape sequences using a switch*/
+    int i, j;
+
+    for (i = 0, j = 0; s[i] != '\0'; i++, j++) {
+        switch (s[i]) {
+            case '\t':
+            case '\n':
+                t[j] = '\\';
+                ++j;
+                t[j] = (s[i] == '\t') ?'t' : 'n';
+                break;
+            default:
+                t[j] = s[i];
+                break;
+        }
+    }
+    t[j] = '\0';
+    printf("s: %s\nt: %s\n",s,t);
 }
 
 int intlen2(int v[]) {
