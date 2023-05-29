3ca4819affa4dd150aaeadc5eda6ef74b4fb77e4
Katherine Turk <kbturk87@gmail.com>
2023-05-28 23:15:52 -0500

    replaced 2d index array with single pointer

--- ch5/cDays/main.c
+++ ch5/cDays/main.c
@@ -39,10 +39,11 @@ int main() {
 
 //set day of year from month & day
 int day_of_year(int year, int month, int day) {
-    int i, leap;
+    int leap;
+    char *p;
 
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
-
+    p = daytab[leap];
     //error checking
     if (month < 1 || month > 12)  {
         printf("Error: %d month outside bounds\n",month);
@@ -52,16 +53,18 @@ int day_of_year(int year, int month, int day) {
         return 0;
     }
 
-    for (i = 1; i < month; i++)
-        day += daytab[leap][i];
+    while (--month)
+        day += *++p;
     return day;
 }
 
 //set month, day from day of year
 void month_day(int year, int yearday, int *pmonth, int *pday) {
-    int i, leap;
+    int leap;
+    char *p;
 
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    p = daytab[leap];
 
     //error checking
     if (yearday < 1 ) {
@@ -69,9 +72,9 @@ void month_day(int year, int yearday, int *pmonth, int *pday) {
         return;
     }
 
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
-    *pmonth = i;
+    while (yearday > *++p)
+        yearday -= *p;
+    *pmonth = p - *(daytab + leap);
     *pday = yearday;
 }
 
