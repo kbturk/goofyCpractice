1056a3cffba8bd077778342306bde0a1c831746a
Katherine Turk <kbturk87@gmail.com>
2023-07-15 20:13:37 -0500

    added -f aka case insensitive sort

--- ch5/psortpro/ex.c
+++ ch5/psortpro/ex.c
@@ -18,20 +18,27 @@ void qsort2(void *lineptr[], int left, int right,
         int (*comp)(void *, void *), int direction);
 void swap(void *v[], int i, int j);
 
-int numcmp(char *, char *);
-int strcmp2(char *, char *);
+int numcmp(char *, char *); //sort numbers stored as strings
+int strcmp2(char *, char *); //case sensitive
+int strcmp3(char *s, char *t); //case insensitive
+
+void tolowercase(char *v); //change line to lowercase
+int tolower(int c);
+
+//yep... it's a global
+int FOLD = 0;
 
 /* sort input lines */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int nlines; //number of linput lines read
     int c;
     int numeric = 0;
     int direction = 1;
-    int fold = 0;
-    
-        while (--argc && ((*++argv)[0] == '-')) {
-            c = (*argv)[1];
-            switch(c) {
+
+    while (--argc && ((*++argv)[0] == '-')) {
+        c = (*argv)[1];
+        switch(c) {
             case 'n':
                 numeric = 1;
                 break;
@@ -39,19 +46,17 @@ int main(int argc, char *argv[]) {
                 direction = -1;
                 break;
             case 'f':
-                fold = 1;
+                FOLD = 1;
                 break;
             default:
                 printf("could not parse: %s", c);
                 break;
-            }
         }
-
-        printf("numeric: %d, direction: %d\n", numeric, direction);
+    }
 
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
         qsort2((void **) lineptr, 0, nlines-1,
-                (int (*)(void*, void*))(numeric ? numcmp : strcmp2),
+                (int (*)(void*, void*))(numeric ? numcmp : FOLD? strcmp3: strcmp2),
                 direction);
         writelines(lineptr, nlines);
         return 0;
@@ -84,23 +89,26 @@ int readlines(char *lineptr[], int maxlines)
 }
 
 //write output lines
-void writelines(char *lineptr[], int nlines) {
+void writelines(char *lineptr[], int nlines)
+{
     while (nlines-- > 0) 
         printf("%s\n", *lineptr++);
 }
 
 /* qsort: sort v[left]...v[right] into order specified by direction*/
 void qsort2(void *v[], int left, int right,
-        int (*comp)(void *, void *), int direction) {
+        int (*comp)(void *, void *), int direction)
+{
     int i, last;
 
     if (left >=right) //do nothing if an array contains
         return;       //fewer than two elements
     swap(v, left, (left + right)/2);
     last = left;
-    for (i = left + 1; i <= right; i++)
-        if (((*comp)(v[i], v[left]))*direction < 0)
-            swap(v, ++last, i);
+    for (i = left + 1; i <= right; i++) {
+    if (((*comp)(v[i], v[left]))*direction < 0)
+        swap(v, ++last, i);
+    }
     swap(v, left, last);
     qsort2(v, left, last -1, comp, direction);
     qsort2(v, last+1, right, comp, direction);
@@ -113,6 +121,9 @@ int numcmp(char *s1, char *s2)
 
     v1 = atof(s1);
     v2 = atof(s2);
+
+    if (v1 == v2 && v1 == 0.00) 
+        return (FOLD? strcmp3(s1,s2) : strcmp2(s1, s2));
 
     if (v1 < v2)
         return -1;
@@ -131,6 +142,16 @@ int strcmp2(char *s, char *t)
     return *s - *t;
 }
 
+//strcmp3: lower and upper case doesn't matter.
+int strcmp3(char *s, char *t)
+{
+    for (; tolower(*s) == tolower(*t); s++, t++)
+        if (*s == '\0')
+            return 0;
+    return tolower(*s) - tolower(*t);
+}
+
+
 void swap(void *v[], int i, int j)
 {
     char *temp;
@@ -138,6 +159,22 @@ void swap(void *v[], int i, int j)
     temp = v[i];
     v[i] = v[j];
     v[j] = temp;
+}
+
+void tolowercase(char *v)
+{
+    while (*v != '\0') {
+        if (*v >= 'A' && *v <= 'Z')
+            *v = *v + 'a' - 'A';
+        v++;
+    }
+}
+
+int tolower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        c = c + 'a' - 'A';
+    return c;
 }
 
 int getline(char s[], int lim)
